b4ec3e5167815b1b48d8b9dcfb225125018c4c87
liju11 <lia15@gmx.ch>
2023-03-14 13:55:05 +0100

    links-rechts-Steuerung

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 var spieler = document.querySelector('.player')
+spieler.style.left = '0px'
 
 function loop() {
+    if(keyboard(39)) {
+        spieler.style.left = parseInt(spieler.style.left) + 5 + 'px'
+    }
+    if(keyboard(37)) {
+        spieler.style.left = parseInt(spieler.style.left) - 5 + 'px'
+    }
     window.requestAnimationFrame(loop)
 }
 
