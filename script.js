e7d3b8714751598bcfd0ddf7b9cde93859e609e4
liju11 <lia15@gmx.ch>
2023-03-21 13:51:56 +0100

    Code für Stein als Hindernis hinzugefügt

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ spieler.style.left = '0px'
 var spielfeld = document.querySelector('.playground')
 var backgroundPosition = 0;
 
+var timer = new Timer(30)
+
+
 function loop() {
     // Background-Scrolling:
     backgroundPosition = backgroundPosition + 5;
@@ -16,6 +19,25 @@ function loop() {
     if(keyboard(37)) {
         spieler.style.left = parseInt(spieler.style.left) - 5 + 'px'
     }
+
+    // Hindernis - Stein:
+    if(timer.ready()) {
+        var h = document.createElement('div')
+        h.classList.add('stein')
+        h.style.top = '0px'
+        h.style.left = '100px'
+        spielfeld.appendChild(h)
+    }
+
+    var steine = document.querySelectorAll('.stein')
+    for(var stein of steine) {
+        stein.style.top = parseInt(stein.style.top) + 5 + 'px'
+        if(parseInt(stein.style.top) > 400) {
+            stein.parentNode.removeChild(stein)
+        }
+    }
+
+
     window.requestAnimationFrame(loop)
 }
 
