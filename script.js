4ef7de46c94c1988c3b9c19da78371c25f07ec87
liju11 <lia15@gmx.ch>
2023-03-21 13:56:02 +0100

    Code für Tore als Hindernisse

--- script.js
+++ script.js
@@ -37,6 +37,40 @@ function loop() {
         }
     }
 
+    // Hindernis - Tor Blau:
+    if(timer.ready()) {
+        var b = document.createElement('div')
+        b.classList.add('tor1')
+        b.style.top = '0px'
+        b.style.left = '100px'
+        spielfeld.appendChild(b)
+    }
+
+    var tore1 = document.querySelectorAll('.tor1')
+    for(var tor1 of tore1) {
+        tor1.style.top = parseInt(tor1.style.top) + 5 + 'px'
+        if(parseInt(tor1.style.top) > 400) {
+            tor1.parentNode.removeChild(tor1)
+        }
+    }
+
+    // Hindernis - Tor Rot:
+    if(timer.ready()) {
+        var r = document.createElement('div')
+        r.classList.add('tor1')
+        r.style.top = '0px'
+        r.style.left = '100px'
+        spielfeld.appendChild(r)
+    }
+
+    var tore2 = document.querySelectorAll('.tor2')
+    for(var tor2 of tore2) {
+        tor2.style.top = parseInt(tor2.style.top) + 5 + 'px'
+        if(parseInt(tor2.style.top) > 400) {
+            tor2.parentNode.removeChild(tor2)
+        }
+    }
+
 
     window.requestAnimationFrame(loop)
 }
