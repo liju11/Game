80ef9f48c976de650f274e27992ad734d3ce0394
Minimischel <minimischelroma@gmail.com>
2023-03-28 14:21:13 +0200

    Es funktioniert woop woop :)

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 var spieler = document.querySelector('.player')
-spieler.style.left = '0px'
+spieler.style.left = '380px'
 
 var spielfeld = document.querySelector('.playground')
 var backgroundPosition = 0;
 
-var timer = new Timer(30)
+var timer_b = new Timer(240)
+var timer_r = new Timer(480)
+var timer_stein = new Timer(240)
+var timer_stern = new Timer(360)
+
+
+
 
 
 function loop() {
@@ -15,59 +21,84 @@ function loop() {
     // Tastatursteuerung links-rechts:
     if(keyboard(39)) {
         spieler.style.left = parseInt(spieler.style.left) + 5 + 'px'
+        spieler.classList.remove('player_left')
     }
+
     if(keyboard(37)) {
         spieler.style.left = parseInt(spieler.style.left) - 5 + 'px'
+        spieler.classList.add('player_left')
     }
 
     // Hindernis - Stein:
-    if(timer.ready()) {
-        var h = document.createElement('div')
-        h.classList.add('stein')
-        h.style.top = '0px'
-        h.style.left = '100px'
-        spielfeld.appendChild(h)
+   if(timer_stein.ready()) {
+        var s = document.createElement('img')
+        s.src="Bilder/Stein.png"
+        s.classList.add('stein')
+        s.style.top = document.body.clientHeight + "px"
+        s.style.left = 25 + Math.random () * 50 + 'vw'
+        spielfeld.appendChild(s)
     }
 
     var steine = document.querySelectorAll('.stein')
     for(var stein of steine) {
-        stein.style.top = parseInt(stein.style.top) + 5 + 'px'
-        if(parseInt(stein.style.top) > 400) {
+        stein.style.top = parseInt(stein.style.top) - 2 + 'px'
+        if(parseInt(stein.style.top) < -100) {
             stein.parentNode.removeChild(stein)
         }
     }
 
     // Hindernis - Tor Blau:
-    if(timer.ready()) {
-        var b = document.createElement('div')
+    if(timer_b.ready()) {
+        var b = document.createElement('img')
+        b.src='Bilder/Tor_blau.png'
         b.classList.add('tor1')
-        b.style.top = '0px'
-        b.style.left = '100px'
+        b.style.top = document.body.clientHeight + "px"
+        b.style.left = 30 + Math.random () * 20 + 'vw'
         spielfeld.appendChild(b)
+        timer_b = new Timer(480)
     }
 
     var tore1 = document.querySelectorAll('.tor1')
     for(var tor1 of tore1) {
-        tor1.style.top = parseInt(tor1.style.top) + 5 + 'px'
-        if(parseInt(tor1.style.top) > 400) {
+        tor1.style.top = parseInt(tor1.style.top) - 2 + 'px'
+        if(parseInt(tor1.style.top) < -100) {
             tor1.parentNode.removeChild(tor1)
         }
     }
 
     // Hindernis - Tor Rot:
-    if(timer.ready()) {
-        var r = document.createElement('div')
-        r.classList.add('tor1')
-        r.style.top = '0px'
-        r.style.left = '100px'
+    if(timer_r.ready()) {
+        var r = document.createElement('img')
+        r.src='Bilder/Tor_rot.png'
+        r.classList.add('tor2')
+        r.style.top = document.body.clientHeight + "px"
+        r.style.left = 50 + Math.random () * 20 + 'vw'
         spielfeld.appendChild(r)
     }
 
     var tore2 = document.querySelectorAll('.tor2')
     for(var tor2 of tore2) {
-        tor2.style.top = parseInt(tor2.style.top) + 5 + 'px'
-        if(parseInt(tor2.style.top) > 400) {
+        tor2.style.top = parseInt(tor2.style.top) - 2 + 'px'
+        if(parseInt(tor2.style.top) < -100) {
             tor2.parentNode.removeChild(tor2)
+        }
+    }
+
+     // Hindernis - Stern:
+     if(timer_stern.ready()) {
+        var w = document.createElement('img')
+        w.src='Bilder/Stern.png'
+        w.classList.add('stern')
+        w.style.top = document.body.clientHeight + "px"
+        w.style.left = 50 + Math.random () * 20 + 'vw'
+        spielfeld.appendChild(w)
+    }
+
+    var sterne = document.querySelectorAll('.stern')
+    for(var stern of sterne) {
+        stern.style.top = parseInt(stern.style.top) - 2 + 'px'
+        if(parseInt(stern.style.top) < -100) {
+            stern.parentNode.removeChild(stern)
         }
     }
 
